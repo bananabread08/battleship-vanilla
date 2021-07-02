7ccb2478cca9fabd3ec113b8e91744a3bb9501e8
bananabread08 <rmkfermin@gmail.com>
2021-07-02 17:09:57 +0800

    added shipFactory test and primary file

---
+++ src/__tests__/shipFactory.test.js
@@ -0,0 +1,47 @@
+import shipFactory from '../factories/shipFactory';
+
+test('Zero ship length', () => {
+  expect(shipFactory(0)).toBe('Length cannot be 0');
+});
+
+test('returns an object', () => {
+  const ship = shipFactory(1);
+  expect(typeof ship).toBe('object');
+});
+
+test('returns ship length', () => {
+  const three = shipFactory(3);
+  const five = shipFactory(5);
+  expect(three.length).toBe(3);
+  expect(five.length).toBe(5);
+});
+
+test('hit with no position', () => {
+  const ship = shipFactory(5);
+  expect(ship.hit()).toBe(undefined);
+});
+
+test('successful Ship hit', () => {
+  const ship = shipFactory(5);
+  ship.hit(2);
+  expect(ship.shipHP).toEqual(['o', 'o', 'hit', 'o', 'o']);
+});
+test('rotate Ship', () => {
+  const ship = shipFactory(3);
+  expect(ship.isRotated(false)).toBe(false);
+  expect(ship.isRotated(true)).toBe(true);
+});
+test('check if Ship is not sunk', () => {
+  const ship = shipFactory(5);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test('succesfull sunk Ship', () => {
+  const ship = shipFactory(5);
+  ship.hit(1);
+  ship.hit(2);
+  ship.hit(0);
+  ship.hit(3);
+  ship.hit(4);
+  expect(ship.isSunk()).toBe(true);
+});
---
+++ src/factories/shipFactory.js
@@ -0,0 +1,46 @@
+const shipFactory = (length) => {
+  // carrier [5], battleship[4], cruiser[3], submarine[2], destroyer[1]
+  let name = '';
+
+  switch (length) {
+    case 6:
+      name = 'carrier';
+      break;
+    case 5:
+      name = 'battleship';
+      break;
+    case 4:
+      name = 'frigate';
+      break;
+    case 3:
+      name = 'submarine';
+      break;
+    case 2:
+      name = 'destroyer';
+      break;
+    case 1:
+      name = 'tanker';
+      break;
+    default:
+      name = '';
+  }
+
+  if (length === undefined || length === 0) return 'Length cannot be 0';
+  const axis = false; // false is horizontal axis
+
+  const shipHP = new Array(length).fill('o'); // Ship life depending on length
+
+  const isRotated = (rotation) => {
+    if (rotation) return !axis;
+    return axis;
+  }; // change rotation
+
+  const hit = (position) => {
+    shipHP.splice(position, 1, 'hit');
+  };
+
+  const isSunk = () => shipHP.every((hp) => hp === 'hit');
+
+  return { length, isRotated, hit, isSunk, shipHP, axis, name };
+};
+export default shipFactory;
