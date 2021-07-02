d9778fb1c7684c864b4a16c77572877dc1a7c766
bananabread08 <rmkfermin@gmail.com>
2021-07-02 17:11:56 +0800

    added playerFactory

---
+++ src/factories/playerFactory.js
@@ -0,0 +1,11 @@
+const playerFactory = (name, board) => {
+  const playerTurn = true;
+
+  const getName = () => name;
+
+  const startGameboard = () => board;
+
+  return { playerTurn, getName, startGameboard };
+};
+
+export default playerFactory;
