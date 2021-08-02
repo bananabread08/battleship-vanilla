910a222bc7d9bcf1958c9cd220aa8fc853c138a7
bananabread08 <rmkfermin@gmail.com>
2021-08-02 14:57:23 +0800

    implemented player and enemyBoard with specified css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-const hello = document.querySelector('.hello');
-hello.style.color = 'red';
+import playerFactory from './factories/playerFactory';
+import gameboardFactory from './factories/gameboardFactory';
+import './styles/style.css';
+
+const playerBoardEl = document.querySelector('.playerboard');
+const enemyBoardEl = document.querySelector('.enemyboard');
+const player = playerFactory('you', gameboardFactory()).startGameboard();
+const enemy = playerFactory('enemy', gameboardFactory()).startGameboard();
+
+player.getBoard().forEach((row) => {
+  const rowBlock = document.createElement('div');
+  rowBlock.classList.add('row');
+  row.forEach((column) => {
+    const columnBlock = document.createElement('div');
+    columnBlock.classList.add('column');
+    rowBlock.appendChild(columnBlock);
+  });
+  playerBoardEl.appendChild(rowBlock);
+});
+
+enemy.getBoard().forEach((row) => {
+  const rowBlock = document.createElement('div');
+  rowBlock.classList.add('row');
+  row.forEach((column) => {
+    const columnBlock = document.createElement('div');
+    columnBlock.classList.add('column');
+    rowBlock.appendChild(columnBlock);
+  });
+  enemyBoardEl.appendChild(rowBlock);
+});
